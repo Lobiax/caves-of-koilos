19b309346f32fad4bdad26c828f7fa6660a66445
Lobiax <>
2023-09-05 10:52:10 +0000

    Created global variables and started work on start and path functions

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+health = 100
+weapon = False
+
+
+def start_game():
+    """
+    Starts the game and prompts the user to type their adventurer's name.
+    """
+    adventurer = input("Please tell us the name of your adventurer")
+    print(f"Greetings {adventurer}, many dangers await you in these caves.\n")
+    print("Please proceed with caution")
+    choose_path()
+
+
+def choose_path():
+    print("Choose the way forward\n")
+    print("There are three doors in front of you. Left, middle and right\n")
+    print("Use the left, up and right keys to choose between the doors")
+    
